04bf9e811b65196330ca1e0bce22062c1796274c
do.urrutia <do.urrutia@duocuc.cl>
2026-05-27 18:40:22 -0400

    NuevaClassDataConSusDatosTest1

---
+++ back-Ventas_SpringBoot/Springboot-API-REST/target/classes/data.sql
@@ -0,0 +1,8 @@
+-- Insertando órdenes de compra de prueba para el sistema de despachos
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) VALUES
+('Avenida Providencia 1234, Depto 45, Providencia', 85000, '2026-05-25', 1),
+('Gran Avenida 5678, Casa 3, San Miguel', 24990, '2026-05-26', 0),
+('Arturo Prat 450, San Bernardo', 15500, '2026-05-26', 1),
+('Avenida Concha y Toro 1011, Local 5, Puente Alto', 120000, '2026-05-27', 0),
+('Calle Falsificada 123, Maipú', 34500, '2026-05-27', 0),
+('Avenida Apoquindo 4567, Oficina 102, Las Condes', 55000, '2026-05-28', 0);
